12c45f26bdbb4532dfc2900568e681b7ef626e41
Michael Bledsoe <michael.bledsoe@gmail.com>
2022-02-09 22:25:54 -0500

    Added a Dapper DAO generator.  Added several more column metadata properties.

---
+++ Source/Upswing/Dapper/DapperDaoModelBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upswing.Dapper
+{
+    public class DapperDaoModelBuilder
+    {
+        private readonly string namespaceName;
+        private readonly INameTransformer nameTransformer;        
+
+        public DapperDaoModelBuilder(string namespaceName, INameTransformer nameTransformer)
+        {
+            this.namespaceName = namespaceName;
+            this.nameTransformer = nameTransformer;
+        }
+
+        public DapperDaoModel Build(TableDefinition tableDefinition)
+        {
+            return new DapperDaoModel
+            {
+                Namespace = namespaceName,
+                Name = $"{nameTransformer.TransformTableName(tableDefinition)}Dao",
+                EntityName = nameTransformer.TransformTableName(tableDefinition),
+                IdentityColumnName = tableDefinition.IdentityColumn.ColumnName,
+                IdentityPropertyName = nameTransformer.TransformColumnName(tableDefinition.IdentityColumn),
+                SelectAllSql = BuildSelectAllSql(tableDefinition),
+                SelectByIdSql = BuildSelectByIdSql(tableDefinition),
+                InsertSql = BuildInsertSql(tableDefinition),
+                UpdateSql = BuildUpdateSql(tableDefinition),
+                DeleteSql = BuildDeleteSql(tableDefinition)
+            };
+        }
+
+        private string BuildSelectByIdSql(TableDefinition tableDefinition)
+        {
+            var selectColumnList = string.Join(",", tableDefinition.Columns.Select(c => $"[{c.ColumnName}]"));
+
+            return $"select {selectColumnList} from [{tableDefinition.SchemaName}].[{tableDefinition.TableName}] where [{tableDefinition.IdentityColumn.ColumnName}] = @{nameTransformer.TransformColumnName(tableDefinition.IdentityColumn)}";
+        }
+
+        private string BuildSelectAllSql(TableDefinition tableDefinition)
+        {
+            var selectColumnList = string.Join(",", tableDefinition.Columns.Select(c => $"[{c.ColumnName}]"));
+
+            return $"select {selectColumnList} from [{tableDefinition.SchemaName}].[{tableDefinition.TableName}] order by [{tableDefinition.IdentityColumn.ColumnName}] offset @Offset rows fetch @PageSize rows only";
+        }
+
+        private string BuildDeleteSql(TableDefinition tableDefinition)
+        {
+            return $"delete from [{tableDefinition.SchemaName}].[{tableDefinition.TableName}] where [{tableDefinition.IdentityColumn.ColumnName}] = @{nameTransformer.TransformColumnName(tableDefinition.IdentityColumn)}";
+        }
+
+        private string BuildUpdateSql(TableDefinition tableDefinition)
+        {
+            var setList = string.Join(",", tableDefinition.MutableColumns.Select(c => $"[{c.ColumnName}] = @{nameTransformer.TransformColumnName(c)}"));
+
+            return $"update [{tableDefinition.SchemaName}].[{tableDefinition.TableName}] set {setList} where [{tableDefinition.IdentityColumn.ColumnName}] = @{nameTransformer.TransformColumnName(tableDefinition.IdentityColumn)}";
+        }
+
+        private string BuildInsertSql(TableDefinition tableDefinition)
+        {
+            var insertColumnList = string.Join(",", tableDefinition.MutableColumns.Select(c => $"[{c.ColumnName}]"));
+            var insertParameterList = string.Join(",", tableDefinition.MutableColumns.Select(c => $"@{nameTransformer.TransformColumnName(c)}"));
+            
+            return $@"insert into [{tableDefinition.SchemaName}].[{tableDefinition.TableName}] ({insertColumnList}) values ({insertParameterList}); select scope_identity();";
+        }
+    }
+}
--- Source/Upswing/SqlServer/ColumnDefinition.cs
+++ Source/Upswing/SqlServer/ColumnDefinition.cs
@@ -7,6 +7,11 @@
         public int ColumnId { get; set; }
         public short SystemTypeId { get; set; }
         public short MaxLength { get; set; }
+        public byte Precision { get; set; }
+        public byte Scale { get; set; }
+        public bool IsRowGuidCol { get; set; }
         public bool IsNullable { get; set; }
+        public bool IsIdentity { get; set; }
+        public bool IsComputed { get; set; }
     }
 }
